6af7e4c484dce6b63dcf40de32d64a5c16b74890
Saulo Román Santillán Nava <romansantillan1998@outlook.com>
2026-07-28 21:27:24 -0600

    bug     : Fixing bugs to deploy

--- src/presentation/services/open-confirmations.service.ts
+++ src/presentation/services/open-confirmations.service.ts
@@ -135,7 +135,13 @@ export class OpenConfirmationsService {
 
     const browser = await puppeteer.launch({
       headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu',
+      ],
     });
 
     try {
